3fa0b4e326e014affad6e95c163c43449bec8401
BoomTHOD <boom6109@gmail.com>
2026-02-19 18:33:13 +0000

    add new funcyion

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
 ## this is my first script
 print("hello world")
-print(" I am using github)
+print("I am using github")
+
+def hello():
+  name = input("what's your name: ")
+  print(f"hello {name}")
+  
+#test
+hello()
